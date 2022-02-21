08ad399d67d495f76d0dc91e1efc415f2d9ad530
Senre <kishorshris@gmail.com>
2022-02-21 11:40:58 +0000

    deployment

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 import { abcCors } from "https://deno.land/x/cors/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const DENO_ENV = Deno.env.get("DENO_ENV") ?? "development";
+
+config({ path: `./.env.${DENO_ENV}`, export: true });
 
 const app = new Application();
-const PORT = 8080;
+const PORT = Deno.env.get("PORT");
 
 const corsInputs = {
   methods: "POST, GET",
@@ -17,3 +22,4 @@ const corsInputs = {
 };
 
 app.use(abcCors(corsInputs));
+app.start({ port: PORT });
