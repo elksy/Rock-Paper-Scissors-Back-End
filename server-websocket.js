b78231e226fedd4592180b795fd3aed55fc7130e
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-22 16:45:25 +0000

    Players are added to the lobby

--- server-websocket.js
+++ server-websocket.js
@@ -21,25 +21,43 @@ const handleWebSocket = async (c, sockets) => {
 
 async function handleEvent(ws, sockets) {
   const uuid = v4.generate(); //generate uuid
-  sockets.set(uuid, ws);
+  sockets.set(uuid, { ws: ws });
   for await (const e of ws) {
     console.log(e);
+    const event = JSON.parse(e);
 
     if (isWebSocketCloseEvent(e)) {
       //checks to see if browser closed
       sockets.delete(uuid); //removed key-value pair from map
     }
-    checkGameStart(e, sockets, uuid);
+    recievedPlayerName(event, sockets, uuid);
+    checkGameStart(event, sockets, uuid);
 
     // ws.send("Sending message");
     // console.log(sockets);
   }
 }
+// ws.send(JSON.stringify({ name: "rob" }));
+function recievedPlayerName(event, sockets, uuid) {
+  if ("name" in event) {
+    const uuidData = sockets.get(uuid);
+    uuidData["playerName"] = event.name;
+    sockets.set(uuid, uuidData);
+    const names = [];
+    const keys = sockets.keys();
+    for (let key of keys) {
+      names.push({ name: sockets.get(key).playerName, uuid: key });
+    }
+    sockets.forEach((obj) => {
+      obj.ws.send(JSON.stringify(names));
+    });
+  }
+}
 
 function checkGameStart(e, sockets, uuid) {
   if (e === "Start the game") {
-    sockets.forEach((ws) => {
-      ws.send("Start the game");
+    sockets.forEach((obj) => {
+      obj.ws.send("Start the game");
     });
   }
 }
