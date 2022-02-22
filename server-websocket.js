e7f283a1e4e4caad3700a1bd5130cd9003356e88
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-22 15:52:53 +0000

    Added start game event

--- server-websocket.js
+++ server-websocket.js
@@ -1,37 +1,47 @@
 import {
-	acceptWebSocket,
-	acceptable,
-	isWebSocketCloseEvent,
+  acceptWebSocket,
+  acceptable,
+  isWebSocketCloseEvent,
 } from "https://deno.land/std@0.99.0/ws/mod.ts";
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const handleWebSocket = async (c, sockets) => {
-	const { conn, headers, r: bufReader, w: bufWriter } = c.request;
-	const ws = await acceptWebSocket({
-		conn,
-		headers,
-		bufReader,
-		bufWriter,
-	}).then((c) => handleEvent(c, sockets));
+  const { conn, headers, r: bufReader, w: bufWriter } = c.request;
+  const ws = await acceptWebSocket({
+    conn,
+    headers,
+    bufReader,
+    bufWriter,
+  }).then((c) => handleEvent(c, sockets));
 
-	// for await (const e of ws) {
-	// 	handleEvent(e);
-	// }
+  // for await (const e of ws) {
+  // 	handleEvent(e);
+  // }
 };
 
 async function handleEvent(ws, sockets) {
-	const uuid = v4.generate(); //generate uuid
-	sockets.set(uuid, ws);
-	for await (const e of ws) {
-		console.log(e);
+  const uuid = v4.generate(); //generate uuid
+  sockets.set(uuid, ws);
+  for await (const e of ws) {
+    console.log(e);
 
-		if (isWebSocketCloseEvent(e)) {
-			//checks to see if browser closed
-			sockets.delete(uuid); //removed key-value pair from map
-		}
+    if (isWebSocketCloseEvent(e)) {
+      //checks to see if browser closed
+      sockets.delete(uuid); //removed key-value pair from map
+    }
+    checkGameStart(e, sockets, uuid);
 
-		console.log(sockets);
-	}
+    // ws.send("Sending message");
+    // console.log(sockets);
+  }
+}
+
+function checkGameStart(e, sockets, uuid) {
+  if (e === "Start the game") {
+    sockets.forEach((ws) => {
+      ws.send("Start the game");
+    });
+  }
 }
 
 export default handleWebSocket;
--- server.js
+++ server.js
@@ -4,31 +4,35 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 import handleWebSocket from "./server-websocket.js";
 
 // Fetches the environment variables to set up the correct PORT
-const DENO_ENV = (await Deno.env.get("DENO_ENV")) ?? "development";
-config({ path: `./.env.${DENO_ENV}`, export: true });
-const PORT = parseInt(Deno.env.get("PORT"));
+// const DENO_ENV = (await Deno.env.get("DENO_ENV")) ?? "development";
+// config({ path: `./.env.${DENO_ENV}`, export: true });
+// const PORT = parseInt(Deno.env.get("PORT"));
 
 // We need to set up a strict cors policy that works for http and websockets
 const CorsConfig = {
-	methods: "GET",
-	origin: "*",
-	allowedHeaders: ["Authorization", "Content-Type", "Accept", "Origin", "User-Agent"],
-	credentials: true,
+  methods: "GET",
+  origin: "*",
+  allowedHeaders: [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Origin",
+    "User-Agent",
+  ],
+  credentials: true,
 };
 
 const app = new Application();
 
 let sockets = new Map();
 
-app.use(abcCors(CorsConfig));
-app.get("/session", (server) => getSession(server));
+app.use(abcCors("*"));
+// app.get("/session", (server) => getSession(server));
 app.get("/ws", (server) => handleWebSocket(server, sockets));
-app.start({ port: PORT });
+app.start({ port: 8080 });
 
 console.log(`server listening on http://localhost:8080`);
 
 function getSession(server) {
-	console.log("session");
+  console.log("session");
 }
-
-sockets.delete(uuid);
