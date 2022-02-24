cb6e7bb81fbde1e51d3ade15d6c9869178740a92
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-24 10:52:43 +0000

    Changed code to use games map instead

--- serverGamepageWs.js
+++ serverGamepageWs.js
@@ -1,53 +1,58 @@
 import {
-	acceptWebSocket,
-	acceptable,
-	isWebSocketCloseEvent,
+   acceptWebSocket,
+   acceptable,
+   isWebSocketCloseEvent,
 } from "https://deno.land/std@0.99.0/ws/mod.ts";
 
 //send opp choice
 
 const handleGamepageWs = async (games, server, sockets, tournaments) => {
-	const uuid = getUserUUID(server);
-	const tournamentID = getTournamentID(server);
-	const { conn, headers, r: bufReader, w: bufWriter } = server.request;
-	const ws = await acceptWebSocket({
-		conn,
-		headers,
-		bufReader,
-		bufWriter,
-	}).then((ws) => handleEvent(games, ws, sockets, tournaments, uuid, tournamentID));
+   const uuid = getUserUUID(server);
+   //const tournamentID = getTournamentID(server);
+   const { conn, headers, r: bufReader, w: bufWriter } = server.request;
+   const ws = await acceptWebSocket({
+      conn,
+      headers,
+      bufReader,
+      bufWriter,
+   }).then((ws) => handleEvent(games, ws, sockets, tournaments, uuid, tournamentID));
 };
 
 async function handleEvent(games, ws, sockets, tournaments, uuid, tournamentID) {
-	// Handle the initial connection. Add the players ws to the sockets map and send them the initial tournament data.
-	addUserSocket(ws, sockets, uuid, tournamentID);
+   // Handle the initial connection. Add the players ws to the sockets map and send them the initial tournament data.
+   addGamesWs(ws, sockets, uuid, tournamentID);
 
-	for await (const e of ws) {
-		if (isWebSocketCloseEvent(e)) {
-			sockets.delete(uuid);
-			sendPlayerInfo(sockets);
-		} else {
-			const event = JSON.parse(e);
-			handlePlayerMove(games, event, uuid, tournaments, tournamentID);
-		}
-	}
+   for await (const e of ws) {
+      if (isWebSocketCloseEvent(e)) {
+         sockets.delete(uuid);
+         sendPlayerInfo(sockets);
+      } else {
+         const event = JSON.parse(e);
+         handlePlayerMove(games, event, uuid, tournaments, tournamentID);
+      }
+   }
 }
 
 function handlePlayerMove(event, uuid, tournaments, tournamentID) {
-	if ("option" in event) {
-		const opponentId = getOpponentId(event, uuid, tournaments, tournamentID);
-		games.get(opponentId).send(event.option);
-	}
+   if ("option" in event) {
+      const opponentId = getOpponentId(event, uuid, tournaments, tournamentID);
+      games.get(opponentId).send(event.option);
+   }
 }
 
-function addUserSocket(ws, sockets, uuid, tournamentID) {
-	const tournamentSockets = sockets.get(tournamentID);
-	tournamentSockets[uuid] = ws;
-	sockets.set(tournamentID, tournamentSockets);
+async function getUserUUID(server) {
+   //   const { uuid } = await server.body;
+   return "33413";
+}
+
+function addGamesWs(ws, uuid) {
+   // const tournamentSockets = sockets.get(tournamentID);
+   // tournamentSockets[uuid] = ws;
+   games.set(uuid, ws);
 }
 
 function getOpponentId(event, uuid, tournaments, tournamentID) {
-	//use uuid to find opp in same tournament bracket
+   //use uuid to find opp in same tournament bracket
 }
 
 export default handleWebSocket;
